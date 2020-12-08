3288908cf2e9edf4840b2030815ba8028d559696
Jo <gototkno1@gmail.com>
2020-12-09 06:13:53 +0900

    Chapter 32 Spring Web Security를 이용한 로그인 처리

---
+++ ex06/src/main/java/org/zerock/security/CustomNoOpPasswordEncoder.java
@@ -0,0 +1,30 @@
+package org.zerock.security;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+public class CustomNoOpPasswordEncoder implements PasswordEncoder{
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		// TODO Auto-generated method stub
+		
+		log.warn("before encdoe : " + rawPassword);
+		
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		// TODO Auto-generated method stub
+		
+		log.warn("matches: " + rawPassword + ":" + encodedPassword);
+		
+		return rawPassword.toString().equals(encodedPassword);
+	}
+	
+	
+
+}
